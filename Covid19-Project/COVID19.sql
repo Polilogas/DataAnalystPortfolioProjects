f9bee3d23a0e617bbee1b85e001635b0d8e5e111
Michail Chourdas <34147163+Polilogas@users.noreply.github.com>
2024-04-21 08:28:02 +0300

    Explore COVID insights! #SQLSkills
    This portfolio showcases database skills with an SQL file and Excel data.

--- Covid19-Project/COVID19.sql
+++ Covid19-Project/COVID19.sql
@@ -5,9 +5,13 @@ Skills used: Joins, CTE's, Windows Functions, Aggregate Functions, Converting Da
 
 */
 
+-- Switch to the destination database 
+USE PortfolioProjects;
+
+
 -- Select Data that we are going to be using
 Select Location, date, total_cases, new_cases, total_deaths, population
-From [COVID19-PortfolioProject]..CovidDeaths
+From ..CovidDeaths
 Where continent IS NOT NULL
 order by 1,2
 
@@ -15,7 +19,7 @@ order by 1,2
 -- Looking at Total Cases vs Total Deaths
 -- Shows the likelihood of dying if you contract COVID-19 in your country
 SELECT location, date, total_cases, total_deaths, (CAST(total_deaths AS DECIMAL) / CAST(total_cases AS DECIMAL)) * 100 AS  death_percentage_of_cases
-FROM [COVID19-PortfolioProject]..CovidDeaths
+FROM ..CovidDeaths
 WHERE location = 'Greece' AND total_cases IS NOT NULL AND continent IS NOT NULL
 ORDER BY 1,2
 
@@ -23,7 +27,7 @@ ORDER BY 1,2
 -- Looking at the Total Cases vs Population
 -- Shows what percentage of the population that got infected with COVID-19
 SELECT location, date, total_cases, population, (total_cases / population) * 100 AS covid_infection_rate
-FROM [COVID19-PortfolioProject]..CovidDeaths
+FROM ..CovidDeaths
 WHERE location = 'Greece' AND total_cases IS NOT NULL AND continent IS NOT NULL
 ORDER BY 1, 2
 
@@ -31,7 +35,7 @@ ORDER BY 1, 2
 -- Countries with the highest infection rate compared to its population
 WITH CTE_CovidCases AS (
 	SELECT location, total_cases, population, (total_cases / population) * 100 AS covid_infection_rate
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE total_cases IS NOT NULL AND continent IS NOT NULL
 )
 SELECT location, MAX(covid_infection_rate) AS highest_infaction_rate
@@ -42,7 +46,7 @@ ORDER BY highest_infaction_rate DESC
 
 -- Showing countries with the highest death count per population
 SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathsCount
-FROM [COVID19-PortfolioProject]..CovidDeaths
+FROM ..CovidDeaths
 WHERE total_deaths IS NOT NULL AND continent IS NOT NULL
 GROUP BY location
 ORDER BY TotalDeathsCount DESC
@@ -50,17 +54,17 @@ ORDER BY TotalDeathsCount DESC
 -- Showing the total deaths from each country compared to their population density
 WITH LatestDatePerCountry AS (
     SELECT location, MAX(date) AS latest_date
-    FROM [COVID19-PortfolioProject]..CovidVaccinations
+    FROM ..CovidVaccinations
     WHERE population_density IS NOT NULL
     GROUP BY location
 )
 
 SELECT vac.location, vac.date, vac.population_density, dea.total_deaths
 FROM LatestDatePerCountry AS ldc
-JOIN [COVID19-PortfolioProject]..CovidVaccinations AS vac
+JOIN ..CovidVaccinations AS vac
     ON ldc.location = vac.location
     AND ldc.latest_date = vac.date
-JOIN [COVID19-PortfolioProject]..CovidDeaths AS dea
+JOIN ..CovidDeaths AS dea
     ON ldc.location = dea.location
     AND ldc.latest_date = dea.date
 ORDER BY vac.population_density DESC;
@@ -70,7 +74,7 @@ ORDER BY vac.population_density DESC;
 
 -- Showing continent's total death count
 SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathsCount
-FROM [COVID19-PortfolioProject]..CovidDeaths
+FROM ..CovidDeaths
 WHERE continent IS NULL AND location NOT LIKE '%income'
 GROUP BY location
 ORDER BY TotalDeathsCount DESC
@@ -83,7 +87,7 @@ SELECT
 	SUM(new_cases) AS total_cases, 
 	SUM(CAST(new_deaths AS INT)) AS total_deaths,
 	SUM(CAST(new_deaths AS INT))/ SUM(new_cases) * 100 AS DeathPercentage
-FROM [COVID19-PortfolioProject]..CovidDeaths
+FROM ..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1, 2
 
@@ -102,7 +106,7 @@ INSERT INTO #HighestInfectedCountries (location, total_cases, population)
 	SELECT location, 
 	MAX(CONVERT(INT, total_cases)) AS total_cases,
 	MAX((total_cases / population) * 100)  AS highest_infaction_rate
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE total_cases IS NOT NULL AND continent IS NOT NULL
 	GROUP BY location, population
 
@@ -117,31 +121,32 @@ ORDER BY population DESC
 -- Showing countries with the highest death count per population
 CREATE VIEW DeathCountPerCountry AS
 	SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathsCount
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE total_deaths IS NOT NULL AND continent IS NOT NULL
 	GROUP BY location
-
 
 
 -- Shows the likelihood of dying if you contract COVID-19 in your country
 CREATE VIEW CaseFatalityRate AS
 	SELECT location, date, total_cases, total_deaths, (CAST(total_deaths AS DECIMAL) / CAST(total_cases AS DECIMAL)) * 100 AS  death_percentage_of_cases
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE total_cases IS NOT NULL AND continent IS NOT NULL
 
 
 -- Shows what percentage of the population that got infected with COVID-19
 CREATE VIEW InfactionRate AS 
 	SELECT location, date, total_cases, population, (total_cases / population) * 100 AS covid_infection_rate
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE total_cases IS NOT NULL AND continent IS NOT NULL
+
 
 -- Showing continent's total death count
 CREATE VIEW ContinentDeathCount AS
 	SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathsCount
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE continent IS NULL AND location NOT LIKE '%income'
 	GROUP BY location
+
 
 -- Total death percentage in the world
 CREATE VIEW WorldCovidData AS
@@ -149,5 +154,5 @@ CREATE VIEW WorldCovidData AS
 		SUM(new_cases) AS total_cases, 
 		SUM(CAST(new_deaths AS INT)) AS total_deaths,
 		SUM(CAST(new_deaths AS INT))/ SUM(new_cases) * 100 AS DeathPercentage
-	FROM [COVID19-PortfolioProject]..CovidDeaths
+	FROM ..CovidDeaths
 	WHERE continent IS NOT NULL
